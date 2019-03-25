726cad1a331540a28da1347c850465ebed79691f
root <root@rhicks-vbcentos7.oss.co.nz>
2019-03-25 15:44:50 +1300

    quieten the messages log

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,6 +44,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Message from Default node": }
+#  notify { "Message from Default node": }
 }
 
